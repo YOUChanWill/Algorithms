6b0a98f35134ac8d4d83c22ea8bca937aad03553
CHAN W1LL <303661031@qq.com>
2023-03-23 22:54:48 +0800

    采用三元组顺序表实现稀疏矩阵的快速转置。

--- datastructures/ArrayList/MatrixQuickTranspose.java
+++ datastructures/ArrayList/MatrixQuickTranspose.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class MatrixQuickTranspose {
 
     public static void main(String[] args) {
-        int i,j,k,l;
         Scanner scan = new Scanner(System.in);
         System.out.println("请输入矩阵的行数，列数，非零元的个数：");
         int mu,nu,tu;
@@ -14,31 +13,31 @@ public class MatrixQuickTranspose {
         tu = scan.nextInt();
         Mat M1 = new Mat(mu,nu,tu);
         Mat M2 = new Mat(nu,mu,tu);
-        System.out.println("请输入矩阵的三元组：");
-        for(i=1; i<=tu; i++){
+        System.out.println("输入矩阵的三元组：");
+        for(int i=1; i<=tu; i++){
             M1.data[i].row = scan.nextInt();
             M1.data[i].col = scan.nextInt();
             M1.data[i].v = scan.nextInt();
         }
-        System.out.println("您输入的矩阵为：");
+        System.out.println("输入的矩阵为：");
         M1.display();
+
         //增加两个向量
         int num[] = new int[nu+1];//M1中第col列中非零元的个数
-        int col,row,t;
-        for(col=1; col<=nu; col++){
+        for(int col=1; col<=nu; col++){
             num[col] = 0;
         }
-        for(t=1; t<=tu; t++){
+        for(int t=1; t<=tu; t++){
             num[M1.data[t].col]++;
         }
         int cpot[] = new int[nu+1];//M1中第col列的第一个非零元在M2.data中的位置。
         cpot[1] = 1;
-        for(col=2; col<=nu; col++){
+        for(int col=2; col<=nu; col++){
             cpot[col] = cpot[col - 1] + num[col - 1];
         }
         int p,q;//实现快速转置
         for(p=1; p<=tu; p++){
-            col = M1.data[p].col;
+            int col = M1.data[p].col;
             q = cpot[col];
             M2.data[q].row = M1.data[p].col;
             M2.data[q].col = M1.data[p].row;
@@ -64,9 +63,9 @@ class Mat {
         for(int i=1; i<=MAXSIZE; i++)
             data[i] = new Triple();
     }
-    // 稀疏矩阵的置转
+    // 矩阵的打印
     public void display(){
-        int i,j,k,m,n,count = 0;
+        int i,j,k,count = 0;
         for(i=1; i<=mu; i++){
             for(j=1; j<=nu; j++){
                 for(k=1; k<=tu; k++){
@@ -85,10 +84,10 @@ class Mat {
     }
 
 }
-
+// 定义三元组
 class Triple<T> {
-    int row,col;
-    T v;
+    int row,col; // 横坐标、纵坐标
+    T v; // 值
     public Triple(){}
     public Triple(int row,int col, T v){
         this.row = row;
