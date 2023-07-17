dec68a96c6d11c973502a230df6f581fa6c638c2
CHAN W1LL <303661031@qq.com>
2023-07-17 23:05:11 +0800

    递归：插入排序

--- algorithms/recursion/InsertionSort.java
+++ algorithms/recursion/InsertionSort.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void sort(int[] list){
-        insertion(list,1);
+        insertion01(list,1);
     }
-    private static void insertion(int[] list,int low){
+    private static void insertion01(int[] list,int low){
         if (low == list.length) return;
 
         int cur = list[low];
@@ -17,8 +17,25 @@ public class InsertionSort {
             i--;
         }
         list[i + 1] = cur;
-        insertion(list, low + 1);
+        insertion01(list, low + 1);
     }
+
+    // 一种开销更大的算法
+    private static void insertion02(int[] list,int low){
+        if (low == list.length) return;
+
+        int i = low - 1;
+
+        while (i >= 0 && list[i] > list[i + 1]){
+            int temp = list[i];
+            list[i] = list[i + 1];
+            list[i + 1] = temp;
+            i--;
+        }
+
+        insertion02(list, low + 1);
+    }
+
     public static void main(String[] args) {
         int[] ints = new int[]{2,5,22,6,1,54,98,3,43,23};
         sort(ints);
