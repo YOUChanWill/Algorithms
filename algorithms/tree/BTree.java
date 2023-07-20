f16341b5dd602e39bd43026dbb08a5743c76982d
CHAN W1LL <303661031@qq.com>
2023-07-20 13:57:48 +0800

    B树：节点定义

---
+++ algorithms/tree/BTree.java
@@ -0,0 +1,48 @@
+package tree;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BTree {
+
+    private static class Node{
+        int[] keys; // 关键字
+        Node[] children; // 孩子
+        int keyNumber;
+        boolean leaf = true;
+        int t;
+
+        public Node(int t) {
+            this.keys = new int[2 * t - 1];
+            this.children = new Node[2 * t];
+            this.t = t;
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(Arrays.copyOfRange(keys,0,keyNumber));
+        }
+
+        Node get(int key){
+            int i = 0;
+            while (i < keyNumber){
+                if (keys[i] == key){
+                    return this;
+                }
+                if (keys[i] > key){
+                    break;
+                }
+                i++;
+            }
+            if (leaf){
+                return null;
+            }
+            return children[i].get(key);
+        }
+
+
+    }
+
+
+
+}
